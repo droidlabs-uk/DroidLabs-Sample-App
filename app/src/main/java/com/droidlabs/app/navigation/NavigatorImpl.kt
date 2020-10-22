24211c8ddb33975dfb38a87922d28d78eb58564b
ludwig <ludwig@droidlabs.co.uk>
2020-10-22 18:58:44 +0200

    ADD: CuvvaActivity.kt

--- app/src/main/java/com/droidlabs/app/navigation/NavigatorImpl.kt
+++ app/src/main/java/com/droidlabs/app/navigation/NavigatorImpl.kt
@@ -1,17 +1,18 @@
-package com.droidlabs.transactionsChallenge.navigation;
+package com.droidlabs.app.navigation;
 
 import android.content.Context
 import android.widget.Toast
 import com.droidlabs.breakingbad.ui.BreakingBadActivity
 import com.droidlabs.core.navigation.NavigationDest
 import com.droidlabs.core.navigation.Navigator
+import com.droidlabs.cuvva.ui.home.CuvvaActivity
 import javax.inject.Inject
 
 class NavigatorImpl @Inject constructor() : Navigator {
     override fun navigateTo(navigationDest: NavigationDest, context: Context) =
         when (navigationDest) {
             NavigationDest.BreakingBad -> BreakingBadActivity.launchActivity(context)
-            NavigationDest.Cuvva -> showToast(context)
+            NavigationDest.Cuvva -> CuvvaActivity.launchActivity(context, )
             NavigationDest.Transactions -> showToast(context)
             NavigationDest.Rx -> showToast(context)
             NavigationDest.Coroutine -> showToast(context)
